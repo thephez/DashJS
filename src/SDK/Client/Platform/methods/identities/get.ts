d8b64d5a13d8bcc2def37ba3643c534f83f9d9d0
thephez <thephez@users.noreply.github.com>
2020-05-14 16:14:02 -0400

    feat: test embedded code samples and includes
    update typedoc options

--- src/SDK/Client/Platform/methods/identities/get.ts
+++ src/SDK/Client/Platform/methods/identities/get.ts
@@ -4,8 +4,21 @@ import {Platform} from "../../Platform";
  * Get identies from the platform
  * 
  * @param {Platform} this - bound instance class
- * @param {string} id - id
- * @returns identites
+ * @param {string} id - id of an identity
+ * @returns identites (or `null` if non-existent)
+ * 
+ * ^^^ From existing DashJS comment
+ * ---------------------
+ * 
+ * ---------------------
+ * **Embedded TypeDoc Sample Code**: 
+ * ```javascript
+ * await client.platform.identities.get('3GegupTgRfdN9JMS8R6QXF3B2VbZtiw63eyudh1oMJAk')
+ * ```
+ * ---------------------
+ * **`include` TypeDoc Example (get.md)**:
+ * 
+ * [[include:platform/identities/get.md]]
  */
 export async function get(this: Platform, id: string): Promise<any> {
     // @ts-ignore
